af2a160cfe8c7b3ad2e71ce9994594df68bfab8e
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-27 22:10:51 +0900

    Create Japanese Cities' Attributes.sql

---
+++ Japanese Cities' Attributes.sql
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=false
+일본의 모든 도시에 대하여 모든 컬럼을 출력하는 문제이다
+
+```sql
+SELECT *
+FROM city c
+WHERE c.countrycode = 'JPN'
+```
