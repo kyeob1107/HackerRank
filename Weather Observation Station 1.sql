05df6ef519c0f012eccd41c38031c691e9d4942e
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-27 22:13:49 +0900

    Create Weather Observation Station 1.sql

---
+++ Weather Observation Station 1.sql
@@ -0,0 +1,11 @@
+#[Weather Observation Station 1 | HackerRank](https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=false)
+
+테이블에서 city와 state를 출력하는 문제이다
+
+```sql
+/*
+Enter your query here.
+*/
+SELECT s.city, s.state
+FROM station s
+```
