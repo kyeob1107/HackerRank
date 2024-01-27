dd3526ea85fc815d6779e169795e8013c0a95f2e
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-27 22:08:19 +0900

    Create Select All.sql

---
+++ Select All.sql
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=false
+그냥 테이블의 모든 컬럼을 출력해주는 문제이다
+이런 문제는 왜 있는지 모르겠다..(제일 첫 문제도 마찬가지로 모든 컬럼에 대해 출력하는 것이지 않았나?(조건은 있었지만) 아마 완전 순서대로 푸는게 아니라서 그런 것일 수도 있겠다는 생각을 하였다)
+
+```sql
+SELECT *
+FROM city c
+```
