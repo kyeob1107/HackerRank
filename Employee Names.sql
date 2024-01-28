902f3053792c8d19abe19a2869e564872636388d
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-28 12:43:53 +0900

    Create Employee Names.sql

---
+++ Employee Names.sql
@@ -0,0 +1,9 @@
+[Employee Names | HackerRank](https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=false)
+
+직원들 이름을 출력하는데 알파벳순으로 정렬하는 문제이다
+
+```sql
+SELECT e.name
+FROM employee e
+ORDER BY e.name;
+```
