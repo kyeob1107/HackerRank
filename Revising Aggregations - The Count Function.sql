c6ee31f863257ed26d30736b84fd754e38607f0d
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-29 10:24:29 +0900

    Create Revising Aggregations - The Count Function.sql

---
+++ Revising Aggregations - The Count Function.sql
@@ -0,0 +1,9 @@
+[Revising Aggregations - The Count Function | HackerRank](https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=false)
+
+인구가 10만보다 많은 도시의 수를 출력하는 문제이다
+
+```sql
+SELECT COUNT(*)
+FROM city c
+WHERE c.population >100000
+```
