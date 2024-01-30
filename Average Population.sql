c6cfcc9f5d9ea6ebee65edb16cc077ed581e95d1
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-30 09:10:35 +0900

    Create Average Population.sql

---
+++ Average Population.sql
@@ -0,0 +1,8 @@
+[Average Population | HackerRank](https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true)
+
+모든 도시의 평균 인구를 한자리수까지 반올림하여 표시하는 문제이다.
+
+```sql
+SELECT ROUND(AVG(c.population))
+FROM city c
+```
