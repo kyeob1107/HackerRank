fd2cf6c87ef93e9795edc7c45b0a6387db25d56f
kyeob1107 <154004299+kyeob1107@users.noreply.github.com>
2024-01-31 09:03:37 +0900

    Create Japan Population.sql

---
+++ Japan Population.sql
@@ -0,0 +1,9 @@
+[Japan Population | HackerRank](https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=false)
+
+일본도시의 총인구를 구하는 문제이다.
+
+```sql
+SELECT SUM(c.population)
+FROM city c
+WHERE c.countrycode = 'JPN'
+```
